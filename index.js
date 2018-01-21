68c4fb6aa0c5fccd8c9cb9d4e68255ce737af5af
Jorge Cano <jacinaustin@gmail.com>
2018-01-21 17:02:25 -0600

    index.html file created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.send(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
