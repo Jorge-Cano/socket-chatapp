abcbde9795cd8ad276d059963e53bbbc7d52b5ea
Jorge Cano <jacinaustin@gmail.com>
2018-01-21 17:07:26 -0600

    serving up index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 
 
 app.get('/', function(req, res){
-  res.send(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
